26bdb75888bdf087976d7f44e83d708eac0ed3ab
Xavier Dooms <dooms.xavier@gmail.com>
2020-10-20 13:14:57 +0200

    add CORS headers

--- go/main.go
+++ go/main.go
@@ -22,5 +22,17 @@ func main() {
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
 
+	router.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Access-Control-Request-Method") != "" {
+			// Set CORS headers
+			header := w.Header()
+			header.Set("Access-Control-Allow-Methods", header.Get("Allow"))
+			header.Set("Access-Control-Allow-Origin", "*")
+		}
+
+		// Adjust status code to 204
+		w.WriteHeader(http.StatusNoContent)
+	})
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
