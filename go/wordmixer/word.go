8301bbc843aee55b1a369e304a472578f41cbc67
Xavier Dooms <dooms.xavier@gmail.com>
2020-10-23 00:33:40 +0200

    fetch random word rune

--- go/wordmixer/word.go
+++ go/wordmixer/word.go
@@ -1,10 +1,28 @@
 package wordmixer
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Word struct {
-	Word string
+	Word  string
+	runes []rune
+	len   int
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
 func NewWord(w string) *Word {
-	wm := Word{w}
+	wm := Word{Word: w}
+	wm.runes = []rune(w)
+	wm.len = len(wm.runes)
 	return &wm
+}
+
+func RandomWordRune(word *Word) rune {
+	random_index := rand.Intn(word.len)
+	return word.runes[random_index]
 }
